da8cd39cc3132c8bbc68f049437faad13666410f
Vattem Leela <Leela.Vattem@fisglobal.com>
2018-11-03 19:48:08 +0530

    changed back path to absolute path

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/heroku-pjkt'));
+app.use(express.static(__dirname + '/dist/heroku-pjkt'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join('./dist/heroku-pjkt/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/heroku-pjkt/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
